51a057dd03d4b388d49603303be27d453235bcdf
GabrielLouka <89856380+GabrielLouka@users.noreply.github.com>
2023-01-23 16:25:00 -0500

    config page main set up

--- client/src/app/pages/configuration-page/configuration-page.component.ts
+++ client/src/app/pages/configuration-page/configuration-page.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./configuration-page.component.scss']
 })
 export class ConfigurationPageComponent implements OnInit {
-
+  title = "Page de configuration";
   constructor() { }
 
   ngOnInit(): void {
