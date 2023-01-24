ae926186d45e0f0bf46847a4bb39f23b6340c5a8
Ibrahim Boulahchiche <ibrahim.boulahchiche@polymtl.ca>
2023-01-23 19:42:55 -0500

    return button implementation

--- client/src/app/pages/configuration-page/configuration-page.component.ts
+++ client/src/app/pages/configuration-page/configuration-page.component.ts
@@ -1,22 +1,23 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
-  selector: 'app-configuration-page',
-  templateUrl: './configuration-page.component.html',
-  styleUrls: ['./configuration-page.component.scss']
+    selector: 'app-configuration-page',
+    templateUrl: './configuration-page.component.html',
+    styleUrls: ['./configuration-page.component.scss'],
 })
 export class ConfigurationPageComponent implements OnInit {
-  title = "Page de configuration";
-  constructor() { }
+    title = 'Page de configuration';
+    constructor(private location: Location) {}
 
-  ngOnInit(): void {
-  }
-  
-  over(){
-    let subBox = document.getElementById("sub-box");
-    if (subBox){
-      subBox.className = "game-buttons";
+    ngOnInit(): void {}
+
+    over() {
+        const subBox = document.getElementById('sub-box');
+        if (subBox) {
+            subBox.className = 'game-buttons';
+        }
     }
-  }
-
+    previousPage() {
+        this.location.back();
+    }
 }
