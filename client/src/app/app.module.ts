447ff1967ebbfedd687e260840ed0ae4693443f5
Marie-Jade Marcil <marie-jade.marcil@polymtl.ca>
2023-01-25 15:49:18 -0500

    good classic path added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,27 +1,18 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-=======
 import { FormsModule } from '@angular/forms';
 import { MatGridListModule } from '@angular/material/grid-list';
->>>>>>> feature/selection-view
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from '@app/modules/app-routing.module';
 import { AppMaterialModule } from '@app/modules/material.module';
 import { AppComponent } from '@app/pages/app/app.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
-<<<<<<< HEAD
-import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
-import { RegistrationPageComponent } from './pages/registration-page/registration-page.component';
-=======
 import { BackButtonComponent } from './components/back-button/back-button.component';
 import { GameCardComponent } from './components/game-card/game-card.component';
 import { NextPageButtonComponent } from './components/next-page-button/next-page-button.component';
 import { OverlayComponent } from './components/overlay/overlay.component';
 import { PreviousPageButtonComponent } from './components/previous-page-button/previous-page-button.component';
->>>>>>> feature/selection-view
 import { SelectionsPageComponent } from './pages/selections-page/selections-page.component';
 
 /**
@@ -33,17 +24,6 @@ import { SelectionsPageComponent } from './pages/selections-page/selections-page
 @NgModule({
     declarations: [
         AppComponent,
-<<<<<<< HEAD
-        GamePageComponent,
-        MainPageComponent,
-        MaterialPageComponent,
-        PlayAreaComponent,
-        SidebarComponent,
-        SelectionsPageComponent,
-        RegistrationPageComponent,
-    ],
-    imports: [AppMaterialModule, AppRoutingModule, BrowserAnimationsModule, BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule],
-=======
         MainPageComponent,
         SelectionsPageComponent,
         BackButtonComponent,
@@ -53,7 +33,6 @@ import { SelectionsPageComponent } from './pages/selections-page/selections-page
         OverlayComponent,
     ],
     imports: [AppMaterialModule, AppRoutingModule, BrowserAnimationsModule, BrowserModule, FormsModule, HttpClientModule, MatGridListModule],
->>>>>>> feature/selection-view
     providers: [],
     bootstrap: [AppComponent],
 })
--- client/src/app/modules/app-routing.module.ts
+++ client/src/app/modules/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClassicPageComponent } from '@app/pages/classic-page/classic-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { SelectionsPageComponent } from '@app/pages/selections-page/selections-page.component';
 
@@ -7,6 +8,7 @@ const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: MainPageComponent },
     { path: 'selections', component: SelectionsPageComponent },
+    { path: 'classic', component: ClassicPageComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
