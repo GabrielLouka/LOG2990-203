7a19ff57748e0d4bed6901fc31f174092aa2c3e2
Marie-Jade Marcil <marie-jade.marcil@polymtl.ca>
2023-01-28 16:51:39 -0500

    images-area component added

---
+++ client/src/app/components/images-area/images-area.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-images-area',
+  templateUrl: './images-area.component.html',
+  styleUrls: ['./images-area.component.scss']
+})
+export class ImagesAreaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
