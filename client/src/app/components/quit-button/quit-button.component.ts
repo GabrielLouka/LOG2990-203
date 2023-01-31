0347f6a092d34f15d8e65b03293f41cc481ca146
Marie-Jade Marcil <marie-jade.marcil@polymtl.ca>
2023-01-30 22:01:13 -0500

    quit-button component created

---
+++ client/src/app/components/quit-button/quit-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quit-button',
+  templateUrl: './quit-button.component.html',
+  styleUrls: ['./quit-button.component.scss']
+})
+export class QuitButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
