c082a3550e038891f4c41ba8274a37557d65e018
Marie-Jade Marcil <marie-jade.marcil@polymtl.ca>
2023-01-30 22:07:51 -0500

    quit button added to classic page

--- client/src/app/components/quit-button/quit-button.component.ts
+++ client/src/app/components/quit-button/quit-button.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-quit-button',
-  templateUrl: './quit-button.component.html',
-  styleUrls: ['./quit-button.component.scss']
+    selector: 'app-quit-button',
+    templateUrl: './quit-button.component.html',
+    styleUrls: ['./quit-button.component.scss'],
 })
-export class QuitButtonComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class QuitButtonComponent {}
