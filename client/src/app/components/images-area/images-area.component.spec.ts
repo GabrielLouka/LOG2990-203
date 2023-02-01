941781514403ab9a58709f38fe872c517935971a
Marie-Jade Marcil <marie-jade.marcil@polymtl.ca>
2023-01-31 23:48:38 -0500

    CSS chat & images added to classic mode page

--- client/src/app/components/images-area/images-area.component.spec.ts
+++ client/src/app/components/images-area/images-area.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ImagesAreaComponent } from './images-area.component';
 
 describe('ImagesAreaComponent', () => {
-  let component: ImagesAreaComponent;
-  let fixture: ComponentFixture<ImagesAreaComponent>;
+    let component: ImagesAreaComponent;
+    let fixture: ComponentFixture<ImagesAreaComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ImagesAreaComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ImagesAreaComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(ImagesAreaComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(ImagesAreaComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- client/src/app/components/mouse/mouse.component.ts
+++ client/src/app/components/mouse/mouse.component.ts
@@ -8,12 +8,25 @@ import { MouseHandlerService } from '@app/services/mouse-handler.service';
     styleUrls: ['./mouse.component.scss'],
 })
 export class MouseComponent {
+    // @ViewChild('canvas', { static: true }) canvas: ElementRef<HTMLCanvasElement>;
+
     // TODO mettre constances dans un fichier
     width = 640;
     height = 480;
-    src = './assets/img/original-picture.png';
+    src = '/Users/Marie-Jade/Desktop/LOG2990/projet2/LOG2990-203/client/src/assets/img/original-picture.png';
 
     constructor(private mouseService: MouseHandlerService) {}
+
+    // ngOnInit() {
+    //     const canvas = this.canvas.nativeElement;
+    //     const ctx = canvas.getContext('2d');
+
+    //     const image = new Image();
+    //     image.src = this.src;
+    //     image.onload = () => {
+    //         ctx?.drawImage(image, 0, 0);
+    //     };
+    // }
 
     onMouseDown(event: MouseEvent) {
         const coordinateClick: Coordinate = { x: event.offsetX, y: Math.abs(event.offsetY - this.height) };
