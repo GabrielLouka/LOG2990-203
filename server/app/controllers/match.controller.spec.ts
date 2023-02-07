2f089525d9586f84065ed617f04b61f7b01668b8
GabrielLouka <89856380+GabrielLouka@users.noreply.github.com>
2023-02-07 15:44:33 -0500

    testing controllers

---
+++ server/app/controllers/match.controller.spec.ts
@@ -0,0 +1,9 @@
+import { StatusCodes } from 'http-status-codes';
+
+const HTTP_STATUS_OK = StatusCodes.OK;
+const HTTP_STATUS_CREATED = StatusCodes.CREATED;
+
+describe("MathController", () => {
+    
+
+});
