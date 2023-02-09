53350dccb82663cacf402806ebc3eb5e462b7845
maaspa <maxime.aspros@polymtl.ca>
2023-02-09 13:34:51 -0500

    removed useless files
    -removed .scss  and test files for config/selection pages
    -removed the logic from the config/selection components

--- client/src/app/pages/configuration-page/configuration-page.component.ts
+++ client/src/app/pages/configuration-page/configuration-page.component.ts
@@ -1,69 +1,7 @@
-// import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
-// import { CommunicationService } from '@app/services/communication.service';
-// import { GameData } from '@common/game-data';
-// import { ImageUploadResult } from '@common/image.upload.result';
-// import { Buffer } from 'buffer';
-// import { BehaviorSubject } from 'rxjs';
+
 @Component({
     selector: 'app-configuration-page',
     templateUrl: './configuration-page.component.html',
-    styleUrls: ['./configuration-page.component.scss'],
 })
 export class ConfigurationPageComponent {}
-//     @Input() isSelection: boolean;
-//     debugDisplayMessage: BehaviorSubject<string> = new BehaviorSubject<string>('');
-//     currentPageNbr: number = 0;
-//     games: {
-//         gameData: GameData;
-//         originalImage: Buffer;
-//     }[];
-
-//     btnType = 'Retour';
-//     title = 'Page de configuration';
-//     playable = false;
-//     gamesNbr: number = 0;
-
-//     showNextButton = true;
-//     showPreviousButton = false;
-//     constructor(private readonly communicationService: CommunicationService) {}
-//     ngOnInit() {
-//         this.title = this.isSelection ? 'Page de configuration ' : 'Page de selection';
-//         this.getGames(this.currentPageNbr);
-//     }
-
-//     async getGames(pageId: number): Promise<void> {
-//         const routeToSend = '/games/' + pageId.toString();
-//         this.communicationService.get(routeToSend).subscribe({
-//             next: (response) => {
-//                 if (response.body !== null) {
-//                     const serverResult = JSON.parse(response.body);
-//                     this.games = serverResult.gameContent;
-//                     this.gamesNbr = serverResult.nbrOfGame;
-//                     this.showNextButton = this.gamesNbr - (this.currentPageNbr + 1) * 4 > 0;
-//                 }
-//             },
-//             error: (err: HttpErrorResponse) => {
-//                 const responseString = `Server Error : ${err.message}`;
-//                 const serverResult: ImageUploadResult = JSON.parse(err.error);
-//                 this.debugDisplayMessage.next(responseString + '\n' + serverResult.message);
-//             },
-//         });
-//     }
-
-//     async goToNextSlide() {
-//         this.currentPageNbr++;
-//         if (this.currentPageNbr > 0) {
-//             this.showPreviousButton = true;
-//         }
-//         await this.getGames(this.currentPageNbr);
-//     }
-
-//     async goToPreviousSlide() {
-//         this.currentPageNbr--;
-//         if (this.currentPageNbr <= 0) {
-//             this.showPreviousButton = false;
-//         }
-//         await this.getGames(this.currentPageNbr);
-//     }
-// }
--- client/src/app/pages/selections-page/selections-page.component.ts
+++ client/src/app/pages/selections-page/selections-page.component.ts
@@ -3,58 +3,5 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-selections-page',
     templateUrl: './selections-page.component.html',
-    styleUrls: ['./selections-page.component.scss'],
 })
 export class SelectionsPageComponent {}
-//     title = 'Page de selection';
-//     playable = true;
-//     debugDisplayMessage: BehaviorSubject<string> = new BehaviorSubject<string>('');
-//     currentPageNbr: number = 0;
-//     games: {
-//         gameData: GameData;
-//         originalImage: Buffer;
-//     }[];
-//     gamesNbr: number = 0;
-
-//     showNextButton = true;
-//     showPreviousButton = false;
-//     constructor(private readonly communicationService: CommunicationService) {}
-//     ngOnInit() {
-//         this.getGames(this.currentPageNbr);
-//     }
-
-//     async getGames(pageId: number): Promise<void> {
-//         const routeToSend = '/games/' + pageId.toString();
-//         this.communicationService.get(routeToSend).subscribe({
-//             next: (response) => {
-//                 if (response.body !== null) {
-//                     const serverResult = JSON.parse(response.body);
-//                     this.games = serverResult.gameContent;
-//                     this.gamesNbr = serverResult.nbrOfGame;
-//                     this.showNextButton = this.gamesNbr - (this.currentPageNbr + 1) * 4 > 0;
-//                 }
-//             },
-//             error: (err: HttpErrorResponse) => {
-//                 const responseString = `Server Error : ${err.message}`;
-//                 const serverResult: ImageUploadResult = JSON.parse(err.error);
-//                 this.debugDisplayMessage.next(responseString + '\n' + serverResult.message);
-//             },
-//         });
-//     }
-
-//     async goToNextSlide() {
-//         this.currentPageNbr++;
-//         if (this.currentPageNbr > 0) {
-//             this.showPreviousButton = true;
-//         }
-//         await this.getGames(this.currentPageNbr);
-//     }
-
-//     async goToPreviousSlide() {
-//         this.currentPageNbr--;
-//         if (this.currentPageNbr <= 0) {
-//             this.showPreviousButton = false;
-//         }
-//         await this.getGames(this.currentPageNbr);
-//     }
-// }
