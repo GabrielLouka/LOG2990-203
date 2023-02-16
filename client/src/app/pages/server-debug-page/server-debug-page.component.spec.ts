d39ba103d31820db6538bab0306a73245f7515bc
antiNT <32984684+antiNT2@users.noreply.github.com>
2023-02-16 10:48:41 -0500

    Revert "removing server debug"
    This reverts commit f8ff11c2b10aaa8ad73a2120f06e3bf1eeef5e1a.

---
+++ client/src/app/pages/server-debug-page/server-debug-page.component.spec.ts
@@ -0,0 +1,22 @@
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { ServerDebugPageComponent } from './server-debug-page.component';
+
+// describe('ServerDebugPageComponent', () => {
+//     let component: ServerDebugPageComponent;
+//     let fixture: ComponentFixture<ServerDebugPageComponent>;
+
+//     beforeEach(async () => {
+//         await TestBed.configureTestingModule({
+//             declarations: [ServerDebugPageComponent],
+//         }).compileComponents();
+
+//         fixture = TestBed.createComponent(ServerDebugPageComponent);
+//         component = fixture.componentInstance;
+//         fixture.detectChanges();
+//     });
+
+//     it('should create', () => {
+//         expect(component).toBeTruthy();
+//     });
+// });
