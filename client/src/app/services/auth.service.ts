2df13cce0da78fb5e06c23e8a00d4e7984a67664
Marie-Jade Marcil <marie-jade.marcil@polymtl.ca>
2023-03-02 09:00:52 -0500

    if usernames are identical, added #1 & #2

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -13,4 +13,16 @@ export class AuthService {
     registeredUserName() {
         return this.userName;
     }
+
+    isIdentical(usernamePlayer1: string, usernamePlayer2: string) {
+        return usernamePlayer1 === usernamePlayer2;
+    }
+
+    userNamesCheck(usernamePlayer1: string, usernamePlayer2: string) {
+        if (this.isIdentical(usernamePlayer1, usernamePlayer2)) {
+            usernamePlayer1 += '#1';
+            usernamePlayer2 += '#2';
+        }
+        return [usernamePlayer1, usernamePlayer2];
+    }
 }
