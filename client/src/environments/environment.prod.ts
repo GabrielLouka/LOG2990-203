ee812b52aa0f43a99544686804807846fd93a5e2
ibrahim Boulahchiche <ibrahim.boulahchiche@polymtl.ca>
2023-03-05 19:45:55 +0000

    Update environment.prod.ts

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    serverUrl: 'ec2-35-183-125-86.ca-central-1.compute.amazonaws.com', // TODO: Add remote server URL
+    serverUrl: 'http://ec2-35-183-125-86.ca-central-1.compute.amazonaws.com', // TODO: Add remote server URL
 };
