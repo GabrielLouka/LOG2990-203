e57f82be5fc0aa3878bb64d76af36341fc1a2c75
Marie-Jade Marcil <marie-jade.marcil@polymtl.ca>
2023-03-19 09:55:49 -0400

    queueManager service implemented
    The queue is now managed by a service.

--- client/src/app/pages/registration-page/registration-page.component.ts
+++ client/src/app/pages/registration-page/registration-page.component.ts
@@ -2,10 +2,10 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '@app/services/auth-service/auth.service';
-import { MatchmakingService } from '@app/services/matchmaking-service/matchmaking.service';
+import { MatchManagerService } from '@app/services/match-manager-service/match-manager.service';
+import { QueueManagerService } from '@app/services/queue-manager-service/queue-manager.service';
 import { SocketClientService } from '@app/services/socket-client-service/socket-client.service';
 import { Match } from '@common/match';
-import { MatchStatus } from '@common/match-status';
 import { Player } from '@common/player';
 import { CLASSIC_PATH, HOME_PATH } from '@common/utils/env.http';
 
@@ -17,13 +17,9 @@ import { CLASSIC_PATH, HOME_PATH } from '@common/utils/env.http';
 export class RegistrationPageComponent implements OnInit, OnDestroy {
     username: string | null | undefined;
     id: string | null;
-    // used to determine if we should display the username field in the page
-    usernameRegistered: boolean;
-    waitingMessage: string = 'Loading';
-    incomingPlayerFound: boolean;
-    incomingPlayer: Player | null = null;
-    hasSentJoinRequest: boolean = false;
-    waitingPlayers: Player[] = [];
+    // used to determine if we should display the username field in the html page
+    hasUsernameRegistered: boolean;
+    hasSentJoinRequest: boolean;
 
     registrationForm = new FormGroup({
         username: new FormControl('', Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z0-9]{3,15}$')])),
@@ -34,93 +30,101 @@ export class RegistrationPageComponent implements OnInit, OnDestroy {
         private auth: AuthService,
         private route: ActivatedRoute,
         private readonly router: Router,
-        private readonly matchmakingService: MatchmakingService,
+        private readonly matchManagerService: MatchManagerService,
+        private readonly queueManagerService: QueueManagerService,
         private readonly socketService: SocketClientService,
-    ) {}
+    ) {
+        this.hasSentJoinRequest = false;
+    }
 
     get user() {
         return this.auth.registeredUsername;
     }
 
+    get hasFoundIncomingPlayer(): boolean {
+        return this.queueManagerService.hasFound;
+    }
+
+    get queueStatusMessage(): string {
+        return this.queueManagerService.queueStatusToDisplay;
+    }
+
     ngOnInit(): void {
         this.id = this.route.snapshot.paramMap.get('id');
-        this.matchmakingService.onGetJoinRequest.add(this.handleIncomingPlayerJoinRequest.bind(this));
-        this.matchmakingService.onGetJoinCancel.add(this.handleIncomingPlayerJoinCancel.bind(this));
-        this.matchmakingService.onGetJoinRequestAnswer.add(this.handleIncomingPlayerJoinRequestAnswer.bind(this));
-        this.matchmakingService.onMatchUpdated.add(this.handleMatchUpdated.bind(this));
+        this.matchManagerService.onGetJoinRequest.add(this.handleIncomingPlayerJoinRequest.bind(this));
+        this.matchManagerService.onGetJoinCancel.add(this.handleIncomingPlayerJoinCancel.bind(this));
+        this.matchManagerService.onGetJoinRequestAnswer.add(this.handleIncomingPlayerJoinRequestAnswer.bind(this));
+        this.matchManagerService.onMatchUpdated.add(this.handleMatchUpdated.bind(this));
         this.signalRedirection();
         this.signalRedirectionOneGame();
     }
 
     ngOnDestroy(): void {
-        if (this.username && this.hasSentJoinRequest) this.matchmakingService.sendMatchJoinCancel(this.username);
+        if (this.username && this.hasSentJoinRequest) this.matchManagerService.sendMatchJoinCancel(this.username);
 
-        this.matchmakingService.onGetJoinRequest.clear();
-        this.matchmakingService.onGetJoinCancel.clear();
-        this.matchmakingService.onGetJoinRequestAnswer.clear();
-        this.matchmakingService.onMatchUpdated.clear();
-    }
-
-    registerUser() {
-        this.auth.registerUser(this.registrationForm.value.username as string);
-        this.username = this.registrationForm.value.username;
-        this.usernameRegistered = true;
-        if (this.matchmakingService.currentMatchPlayed) {
-            if (this.username && this.matchmakingService.is1vs1Mode) {
-                this.matchmakingService.setCurrentMatchPlayer(this.username + '#1');
-            } else if (this.username && this.matchmakingService.isSoloMode) {
-                this.matchmakingService.setCurrentMatchPlayer(this.username);
-            } else window.alert('Username to register is not valid !');
-            if (this.matchmakingService.isSoloMode) {
-                this.loadGamePage();
-            } else {
-                this.waitingMessage = "En attente d'un adversaire...";
-            }
-        } else {
-            this.sendMatchJoinRequest();
-        }
+        this.matchManagerService.onGetJoinRequest.clear();
+        this.matchManagerService.onGetJoinCancel.clear();
+        this.matchManagerService.onGetJoinRequestAnswer.clear();
+        this.matchManagerService.onMatchUpdated.clear();
     }
 
     loadGamePage() {
         this.router.navigate([CLASSIC_PATH, this.id]);
     }
 
-    sendMatchJoinRequest() {
-        this.hasSentJoinRequest = true;
-        this.waitingMessage = "En attente de la réponse de l'adversaire...";
-        if (this.username) this.matchmakingService.sendMatchJoinRequest(this.username + '#2');
+    registerUser() {
+        this.auth.registerUser(this.registrationForm.value.username as string);
+        this.username = this.registrationForm.value.username;
+        this.hasUsernameRegistered = true;
+
+        if (this.matchManagerService.currentMatchPlayed) {
+            if (this.username && this.matchManagerService.is1vs1Mode) {
+                this.matchManagerService.currentMatchPlayer = this.username;
+            } else if (this.username && this.matchManagerService.isSoloMode) {
+                this.matchManagerService.currentMatchPlayer = this.username;
+            } else window.alert('Username to register is not valid !');
+
+            if (this.matchManagerService.isSoloMode) {
+                this.loadGamePage();
+            } else {
+                this.queueManagerService.queueMessage = "En attente d'un adversaire...";
+            }
+        } else {
+            this.sendMatchJoinRequest();
+        }
     }
 
     handleIncomingPlayerJoinRequest(playerThatWantsToJoin: Player) {
-        if (!this.matchmakingService.isHost) return;
-
-        if (!this.waitingPlayers.includes(playerThatWantsToJoin)) {
-            this.waitingPlayers.push(playerThatWantsToJoin);
-        }
-
-        this.refreshQueueDisplay();
+        this.queueManagerService.handleIncomingPlayerJoinRequest(playerThatWantsToJoin);
     }
 
     handleIncomingPlayerJoinCancel(playerIdThatCancelledTheirJoinRequest: string) {
-        if (!this.matchmakingService.isHost) return;
-        this.waitingPlayers = this.waitingPlayers.filter((player) => player.playerId !== playerIdThatCancelledTheirJoinRequest);
-        this.refreshQueueDisplay();
+        this.queueManagerService.handleIncomingPlayerJoinCancel(playerIdThatCancelledTheirJoinRequest);
     }
 
-    refreshQueueDisplay() {
-        this.incomingPlayerFound = this.waitingPlayers.length >= 1;
-        if (this.incomingPlayerFound) {
-            this.waitingMessage = `Voulez-vous débuter la partie avec ${this.waitingPlayers[0].username}?\n`;
-            this.waitingMessage += ' | Joueur(s) en attente : ';
-            for (const player of this.waitingPlayers) {
-                this.waitingMessage += ` ${player.username} \n ,`;
-            }
-
-            this.incomingPlayer = this.waitingPlayers[0];
-        } else {
-            this.waitingMessage = "En attente d'un adversaire";
-            this.incomingPlayer = null;
+    handleIncomingPlayerJoinRequestAnswer(data: { matchId: string; player: Player; isAccepted: boolean }) {
+        if (
+            this.queueManagerService.isAcceptedByHost(data.isAccepted, data.player) ||
+            this.queueManagerService.isHostAcceptingIncomingPlayer(data.isAccepted)
+        ) {
+            this.loadGamePage();
         }
+
+        if (this.queueManagerService.isHostRejectingIncomingPlayer(data.isAccepted)) {
+            this.queueManagerService.handleHostRejectingIncomingPlayer();
+            if (this.queueManagerService.hasIncomingPlayer) {
+                this.queueManagerService.handleIncomingPlayerJoinRequest(this.queueManagerService.firstPlayerInQueue);
+            }
+        }
+        if (this.queueManagerService.isRejectedByHost(data.isAccepted, data.player)) {
+            this.router.navigate(['/']);
+        }
+    }
+
+    sendMatchJoinRequest() {
+        this.hasSentJoinRequest = true;
+        this.queueManagerService.queueMessage = "En attente de la réponse de l'adversaire...";
+        if (this.username) this.matchManagerService.sendMatchJoinRequest(this.username + '#2');
     }
 
     signalRedirection() {
@@ -129,7 +133,7 @@ export class RegistrationPageComponent implements OnInit, OnDestroy {
             const lastSegment = pathSegments[pathSegments.length - 2];
             if (lastSegment === 'registration') {
                 this.router.navigate([HOME_PATH]).then(() => {
-                    window.alert("Le jeu a été supprimé, vous avez donc été redirigé à l'accueil");
+                    alert("Le jeu a été supprimé, vous avez donc été redirigé à l'accueil");
                 });
             }
         });
@@ -142,74 +146,27 @@ export class RegistrationPageComponent implements OnInit, OnDestroy {
             const idPage = pathSegments[pathSegments.length - 1];
             if (pageName === 'registration' && data.id === idPage) {
                 this.router.navigate([HOME_PATH]).then(() => {
-                    window.alert("Le jeu a été supprimé, vous avez donc été redirigé à l'accueil");
+                    alert("Le jeu a été supprimé, vous avez donc été redirigé à l'accueil");
                 });
             }
         });
     }
 
-    isPlayer2(player: Player): boolean {
-        return player.playerId === this.matchmakingService.currentSocketId;
-    }
-
-    isAcceptedByHost(isAccepted: boolean, player: Player): boolean {
-        return isAccepted && this.isPlayer2(player);
-    }
-
-    isRejectedByHost(isAccepted: boolean, player: Player): boolean {
-        return !isAccepted && this.isPlayer2(player);
-    }
-
-    isHostAcceptingPlayer2(isAccepted: boolean): boolean {
-        return isAccepted && this.matchmakingService.isHost;
-    }
-
-    isHostRejectingPlayer2(isAccepted: boolean): boolean {
-        return !isAccepted && this.matchmakingService.isHost && this.waitingPlayers.length >= 1;
-    }
-
-    handleIncomingPlayerJoinRequestAnswer(data: { matchId: string; player: Player; isAccepted: boolean }) {
-        if (this.isAcceptedByHost(data.isAccepted, data.player)) {
-            this.loadGamePage();
-        }
-        if (this.isHostAcceptingPlayer2(data.isAccepted)) {
-            this.loadGamePage();
-        }
-        if (this.isHostRejectingPlayer2(data.isAccepted)) {
-            this.waitingMessage = "En attente d'un adversaire...";
-            this.incomingPlayerFound = false;
-            this.waitingPlayers = this.waitingPlayers.splice(1, this.waitingPlayers.length);
-            if (this.waitingPlayers.length >= 1) {
-                this.handleIncomingPlayerJoinRequest(this.waitingPlayers[0]);
-            }
-        }
-        if (this.isRejectedByHost(data.isAccepted, data.player)) {
-            this.router.navigate(['/']);
-        }
-    }
-
     handleMatchUpdated(match: Match | null) {
         if (!match) return;
-        if (!this.matchmakingService.isHost) {
-            if (match.matchStatus === MatchStatus.Aborted) {
+
+        if (!this.matchManagerService.isHost) {
+            if (this.matchManagerService.isMatchAborted(match)) {
                 this.router.navigate(['/']);
             }
         }
     }
 
     acceptIncomingPlayer() {
-        if (!this.incomingPlayer) return;
-        this.matchmakingService.sendIncomingPlayerRequestAnswer(this.incomingPlayer, true);
-        for (const player of this.waitingPlayers) {
-            if (player !== this.incomingPlayer) {
-                this.matchmakingService.sendIncomingPlayerRequestAnswer(player, false);
-            }
-        }
-        this.waitingPlayers = [this.incomingPlayer];
+        this.queueManagerService.acceptIncomingPlayer();
     }
 
     refuseIncomingPlayer() {
-        if (!this.incomingPlayer) return;
-        this.matchmakingService.sendIncomingPlayerRequestAnswer(this.incomingPlayer, false);
+        this.queueManagerService.refuseIncomingPlayer();
     }
 }
---
+++ client/src/app/services/queue-manager-service/queue-manager.service.ts
@@ -0,0 +1,134 @@
+import { Injectable } from '@angular/core';
+import { MatchManagerService } from '@app/services/match-manager-service/match-manager.service';
+import { Player } from '@common/player';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class QueueManagerService {
+    private waitingPlayers: Player[];
+    private incomingPlayer: Player | null;
+    private queueStatusMessage: string;
+    private hasFoundIncomingPlayer: boolean;
+
+    constructor(private readonly matchManagerService: MatchManagerService) {
+        this.queueStatusMessage = 'Loading';
+        this.incomingPlayer = null;
+        this.waitingPlayers = [];
+        this.handleIncomingPlayerJoinRequest = this.handleIncomingPlayerJoinRequest.bind(this);
+        this.handleIncomingPlayerJoinCancel = this.handleIncomingPlayerJoinCancel.bind(this);
+    }
+
+    get playersInQueue(): Player[] {
+        return this.waitingPlayers;
+    }
+
+    get nbrOfPlayersInQueue(): number {
+        return this.playersInQueue.length;
+    }
+
+    get isEmptyQueue() {
+        return this.waitingPlayers.length === 0;
+    }
+
+    get hasIncomingPlayer(): boolean {
+        return this.nbrOfPlayersInQueue >= 1;
+    }
+
+    get firstPlayerInQueue(): Player {
+        return this.waitingPlayers[0];
+    }
+
+    get queueStatusToDisplay(): string {
+        return this.queueStatusMessage;
+    }
+
+    get hasFound(): boolean {
+        return this.hasFoundIncomingPlayer;
+    }
+
+    set queueMessage(message: string) {
+        this.queueStatusMessage = message;
+    }
+
+    isIncomingPlayer(player: Player): boolean {
+        return player.playerId === this.matchManagerService.currentSocketId;
+    }
+
+    isAcceptedByHost(isAccepted: boolean, player: Player): boolean {
+        return isAccepted && this.isIncomingPlayer(player);
+    }
+
+    isRejectedByHost(isAccepted: boolean, player: Player): boolean {
+        return !isAccepted && this.isIncomingPlayer(player);
+    }
+
+    isHostAcceptingIncomingPlayer(isAccepted: boolean): boolean {
+        return isAccepted && this.matchManagerService.isHost;
+    }
+
+    isHostRejectingIncomingPlayer(isAccepted: boolean): boolean {
+        return !isAccepted && this.matchManagerService.isHost && this.waitingPlayers.length >= 1;
+    }
+
+    refreshQueueDisplay() {
+        this.hasFoundIncomingPlayer = this.hasIncomingPlayer;
+        if (this.hasFoundIncomingPlayer) {
+            const startingGameMessage = 'Voulez-vous débuter la partie ';
+
+            this.queueStatusMessage = startingGameMessage + `avec ${this.firstPlayerInQueue.username}?\n`;
+            this.queueStatusMessage += ' | Joueur(s) en attente : ';
+
+            for (const player of this.waitingPlayers) {
+                this.queueStatusMessage += ` ${player.username} \n ,`;
+            }
+
+            this.incomingPlayer = this.firstPlayerInQueue;
+        } else {
+            this.queueStatusMessage = "En attente d'un adversaire";
+            this.incomingPlayer = null;
+        }
+    }
+
+    handleIncomingPlayerJoinRequest(playerThatWantsToJoin: Player) {
+        if (!this.matchManagerService.isHost) return;
+
+        if (!this.waitingPlayers.includes(playerThatWantsToJoin)) {
+            this.waitingPlayers.push(playerThatWantsToJoin);
+        }
+
+        this.refreshQueueDisplay();
+    }
+
+    handleIncomingPlayerJoinCancel(playerIdThatCancelledTheirJoinRequest: string) {
+        if (!this.matchManagerService.isHost) return;
+
+        this.waitingPlayers = this.waitingPlayers.filter((player) => player.playerId !== playerIdThatCancelledTheirJoinRequest);
+        this.refreshQueueDisplay();
+    }
+
+    handleHostRejectingIncomingPlayer() {
+        this.queueStatusMessage = "En attente d'un adversaire...";
+        this.hasFoundIncomingPlayer = false;
+        this.waitingPlayers.shift();
+    }
+
+    acceptIncomingPlayer() {
+        if (!this.incomingPlayer) return;
+
+        this.matchManagerService.sendIncomingPlayerRequestAnswer(this.incomingPlayer, true);
+
+        for (const player of this.waitingPlayers) {
+            if (player !== this.incomingPlayer) {
+                this.matchManagerService.sendIncomingPlayerRequestAnswer(player, false);
+            }
+        }
+        this.waitingPlayers = [this.incomingPlayer];
+    }
+
+    refuseIncomingPlayer() {
+        if (!this.incomingPlayer) return;
+
+        this.matchManagerService.sendIncomingPlayerRequestAnswer(this.incomingPlayer, false);
+    }
+}
