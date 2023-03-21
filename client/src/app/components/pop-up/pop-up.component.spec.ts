9ff0252130301f15c85a20eaf474a91c4002f613
maaspa <90349318+maaspa@users.noreply.github.com>
2023-03-21 15:27:36 -0400

    tested pop-up

--- client/src/app/components/pop-up/pop-up.component.spec.ts
+++ client/src/app/components/pop-up/pop-up.component.spec.ts
@@ -1,23 +1,61 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PopUpComponent } from './pop-up.component';
 
 describe('PopUpComponent', () => {
-  let component: PopUpComponent;
-  let fixture: ComponentFixture<PopUpComponent>;
+    let component: PopUpComponent;
+    let fixture: ComponentFixture<PopUpComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PopUpComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [PopUpComponent],
+        }).compileComponents();
+    });
 
-    fixture = TestBed.createComponent(PopUpComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(PopUpComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create the component', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should show confirmation pop-up correctly', () => {
+        component.showConfirmationPopUp();
+        expect(component.popUpInfo.length).toEqual(1);
+        expect(component.popUpInfo[0].title).toEqual('VOULEZ-VOUS VRAIMENT QUITTER ?');
+        expect(component.popUpInfo[0].option1).toEqual('OUI');
+        expect(component.popUpInfo[0].option2).toEqual('NON');
+        expect(component.popUpInfo[0].isConfirmation).toBeTruthy();
+        expect(component.popUpInfo[0].isGameOver).toBeFalsy();
+        expect(component.modal.nativeElement.style.display).toEqual('flex');
+    });
+
+    it('should show game over pop-up according to isWinByDefault and isSoloMode values (pt.1)', () => {
+        component.showGameOverPopUp('Test User', false, true);
+        expect(component.popUpInfo.length).toEqual(1);
+        expect(component.popUpInfo[0].title).toContain('Félicitations TEST USER vous avez remporté !');
+        expect(component.popUpInfo[0].option1).toEqual('Menu Principal');
+        expect(component.popUpInfo[0].option2).toEqual('Reprise Vidéo');
+        expect(component.popUpInfo[0].isConfirmation).toBeFalsy();
+        expect(component.popUpInfo[0].isGameOver).toBeTruthy();
+        expect(component.modal.nativeElement.style.display).toEqual('flex');
+    });
+
+    it('should show game over pop-up according to isWinByDefault and isSoloMode values (pt.2)', () => {
+        component.showGameOverPopUp('Test User', true, false);
+        expect(component.popUpInfo.length).toEqual(1);
+        expect(component.popUpInfo[0].title).toContain('Félicitations TEST USER vous avez remporté !');
+        expect(component.popUpInfo[0].option1).toEqual('Menu Principal');
+        expect(component.popUpInfo[0].option2).toEqual('Reprise Vidéo');
+        expect(component.popUpInfo[0].isConfirmation).toBeFalsy();
+        expect(component.popUpInfo[0].isGameOver).toBeTruthy();
+        expect(component.modal.nativeElement.style.display).toEqual('flex');
+    });
+
+    it('should close pop-up correctly', () => {
+        component.closePopUp();
+        expect(component.modal.nativeElement.style.display).toEqual('none');
+    });
 });
