92fa997a96f4d50c976f326dabc8a5021315c780
antiNT <32984684+antiNT2@users.noreply.github.com>
2023-03-21 23:50:17 -0400

    Revert "Update environment.prod.ts"
    This reverts commit ee812b52aa0f43a99544686804807846fd93a5e2.

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    serverUrl: 'http://ec2-35-183-125-86.ca-central-1.compute.amazonaws.com', // TODO: Add remote server URL
+    serverUrl: 'ec2-35-183-125-86.ca-central-1.compute.amazonaws.com', // TODO: Add remote server URL
 };
