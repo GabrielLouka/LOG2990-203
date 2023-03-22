fd46ff1ab91a9b98e4178fc378253d159023abef
ibrahim Boulahchiche <ibrahim.boulahchiche@polymtl.ca>
2023-03-22 02:59:03 +0000

    Update environment.prod.ts

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    serverUrl: 'http://ec2-35-183-123-137.ca-central-1.compute.amazonaws.com/api',
+    serverUrl: 'http://ec2-35-183-123-137.ca-central-1.compute.amazonaws.com:3000/api',
     serverSocketUrl: 'http://ec2-35-183-123-137.ca-central-1.compute.amazonaws.com:3000',
 };
