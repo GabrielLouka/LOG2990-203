9cf0492822eb37835de276606a900950172db29e
antiNT <32984684+antiNT2@users.noreply.github.com>
2023-03-23 14:45:36 -0400

    crash fix

--- server/app/services/matching-difference-service/matching-differences.service.ts
+++ server/app/services/matching-difference-service/matching-differences.service.ts
@@ -6,6 +6,8 @@ import { Service } from 'typedi';
 @Service()
 export class MatchingDifferencesService {
     getDifferenceIndex(game: GameData, clickPosition: Vector2): number {
+        if (!game || !game.differences) return NOT_FOUND;
+
         const differences = game.differences;
         for (let i = 0; i < differences.length; i++) {
             const difference = differences[i];
