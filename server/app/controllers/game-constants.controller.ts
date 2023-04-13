e77942866a19e97e63ff3783643306b130142f79
maaspa <maxime.aspros@polymtl.ca>
2023-04-13 12:55:16 -0400

    removed useless await

--- server/app/controllers/game-constants.controller.ts
+++ server/app/controllers/game-constants.controller.ts
@@ -15,7 +15,8 @@ export class GameConstantsController {
 
         this.router.get('/', async (req: Request, res: Response) => {
             try {
-                const constants = await this.gameConstantsService.getConstants();
+                // const constants = await this.gameConstantsService.getConstants();
+                const constants = this.gameConstantsService.getConstants();
                 res.send(JSON.stringify(constants));
             } catch (error) {
                 res.status(StatusCodes.NOT_FOUND).send(error.message);
