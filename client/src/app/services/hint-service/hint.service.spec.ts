634da812b9bdcecd46566fe28cd58f87b6f69b64
GabrielLouka <89856380+GabrielLouka@users.noreply.github.com>
2023-04-15 15:29:50 -0400

    hint service 60%

--- client/src/app/services/hint-service/hint.service.spec.ts
+++ client/src/app/services/hint-service/hint.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ChatComponent } from '@app/components/chat/chat.component';
 import { GameConstantsService } from '../game-constants-service/game-constants.service';
 import { ImageManipulationService } from '../image-manipulation-service/image-manipulation.service';
 import { HintService } from './hint.service';
@@ -8,13 +9,14 @@ import { HintService } from './hint.service';
 describe('HintService', () => {
   let hintService: HintService;
   let imageService: jasmine.SpyObj<ImageManipulationService>;
+  let chatComponent: ChatComponent;
 //   let constantService: jasmine.SpyObj<GameConstantsService>;
 
   beforeEach(() => {
 
     const spyImageManipulationService = jasmine.createSpyObj('ImageManipulationService', ['showFirstHint', 'showSecondHint', 'showThirdHint']);
     const spyGameConstantsService = jasmine.createSpyObj('GameConstantsService', ['initGameConstants']);
-
+    chatComponent = jasmine.createSpyObj('ChatComponent', ['sendSystemMessage']);
     hintService = new HintService(spyImageManipulationService, spyGameConstantsService);
     imageService = spyImageManipulationService as jasmine.SpyObj<ImageManipulationService>;
     // constantService = spyGameConstantsService as jasmine.SpyObj<GameConstantsService>;
@@ -22,8 +24,8 @@ describe('HintService', () => {
     TestBed.configureTestingModule({
         imports: [HttpClientTestingModule],
         providers: [ImageManipulationService, GameConstantsService, HintService],
-    });
-    hintService = TestBed.inject(HintService);
+        });
+        hintService = TestBed.inject(HintService);
   });
 
   it('should be created', () => {
@@ -51,5 +53,68 @@ describe('HintService', () => {
         const isLimited = false;        
         expect(hintService.getTimePenalty(isLimited)).toEqual(-0);
     });
+
+    it("decrement should decrement", () => {
+        hintService.decrement();
+        expect(hintService.maxGivenHints).toEqual(2);
+    });
+
+    it("returnDisplay should return display", () => {
+        const display = "newDisplay";
+        hintService.returnDisplay(display);
+        expect(display).toBeDefined();
+    });
+
+    it('should send a system message to the chat component with the current time and message', () => {
+        const now = new Date();
+        const formattedTime = now.toLocaleTimeString('en-US', { hour12: false }) + ' - Indice utilisé';    
+        hintService.sendHintMessage(chatComponent);
+    
+        expect(chatComponent.sendSystemMessage).toHaveBeenCalledWith(formattedTime);
+      });
+
+
+
+
+
+    // it('should call showFirstHint method of ImageManipulationService when hints value is 3', () => {
+    //     const canvas = { nativeElement: document.createElement('canvas') };
+    //     const context = canvas.nativeElement.getContext('2d')!;
+    //     const image = Buffer.alloc(100, 1);
+    //     const otherImage = Buffer.alloc(100, 1);
+    //     const gameInfo = {
+    //     gameData: {
+    //         id: 1,
+    //         name: 'Test Game',
+    //         isEasy: true,
+    //         nbrDifferences: 3,
+    //         differences: [
+    //         [
+    //             { x: 10, y: 20 },
+    //             { x: 30, y: 40 },
+    //         ],
+    //         [
+    //             { x: 50, y: 60 },
+    //             { x: 70, y: 80 },
+    //             { x: 90, y: 100 },
+    //         ],
+    //         [
+    //             { x: 110, y: 120 },
+    //             { x: 130, y: 140 },
+    //             { x: 150, y: 160 },
+    //         ],
+    //         ],
+    //         oneVersusOneRanking: [],
+    //         soloRanking: [],
+    //     },
+    //     hints: 2,
+    //     diffs: [true, false, true],
+    //     };
+        
+    //     hintService.showHint(canvas, context, image, otherImage, gameInfo);
+
+    //     expect(imageService.showSecondHint).not.toHaveBeenCalled();
+    //     expect(imageService.showThirdHint).not.toHaveBeenCalled();
+    // });
 
 });
