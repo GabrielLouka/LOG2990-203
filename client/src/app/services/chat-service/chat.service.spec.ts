c4f0005a0987faf167e68f1f11961933701b6198
GabrielLouka <89856380+GabrielLouka@users.noreply.github.com>
2023-04-15 14:30:43 -0400

    80%

--- client/src/app/services/chat-service/chat.service.spec.ts
+++ client/src/app/services/chat-service/chat.service.spec.ts
@@ -5,6 +5,7 @@
 // import { SocketClientService } from '../socket-client-service/socket-client.service';
 
 import { TestBed } from '@angular/core/testing';
+import { ChatComponent } from '@app/components/chat/chat.component';
 import { MatchmakingService } from '@app/services/matchmaking-service/matchmaking.service';
 import { SocketClientService } from '@app/services/socket-client-service/socket-client.service';
 import { ChatService } from './chat.service';
@@ -13,6 +14,7 @@ describe('ChatService', () => {
     let chatService: ChatService;
     let socketServiceSpy: jasmine.SpyObj<SocketClientService>;
     let matchmakingService: MatchmakingService;
+    let chatComponent: ChatComponent;
 
     beforeEach(() => {
         // const spy = jasmine.createSpyObj('SocketClientService', ['socket']);
@@ -37,32 +39,8 @@ describe('ChatService', () => {
         chatService = TestBed.inject(ChatService);
         socketServiceSpy = TestBed.inject(SocketClientService) as jasmine.SpyObj<SocketClientService>;
         matchmakingService = TestBed.inject(MatchmakingService) as jasmine.SpyObj<MatchmakingService>;
+        chatComponent = new ChatComponent(chatService);
     });
-
-    // it('should return true if socketId is equal to player1Id', () => {
-    //     spyOn<any>(socketServiceSpy, 'socketId').and.returnValue('123');
-    //     spyOn<any>(matchmakingService, 'player1Id').and.returnValue('123');
-    //     expect(chatService.isPlayer1).toBeTruthy();
-    // });
-
-    // it('sendMessageFromSystem', () => {
-    //     // const chatElements = {
-    //     //     message: 'Hello world!',
-    //     //     chat: new ElementRef(document.createElement('div')),
-    //     //     newMessage: '',
-    //     // };
-    //     const messages: string | any[] = [];
-
-    //     chatService.sendMessageFromSystem('hi', 'bye', new ChatComponent(chatService));
-
-    //     expect(messages.length).toBe(1);
-    //     expect(messages[0].text).toBe('Hello world!');
-    //     expect(messages[0].username).toBe('System');
-    //     expect(messages[0].sentBySystem).toBe(true);
-    //     expect(messages[0].sentByPlayer1).toBe(false);
-    //     expect(messages[0].sentByPlayer2).toBe(false);
-    //     expect(typeof messages[0].sentTime).toBe('number');
-    // });
 
     it('isPlayer1 getter', () => {
         chatService.isPlayer1;
@@ -104,28 +82,9 @@ describe('ChatService', () => {
         expect(res).toEqual('');
     });
 
-    // it("pushMessage should add message", () => {
-    //     const message = {
-    //         text: "okok",
-    //         username: 'okokok',
-    //         sentBySystem: false,
-    //         sentByPlayer1: true,
-    //         sentByPlayer2: false,
-    //         sentUpdatedScore: false,
-    //         sentTime: 2
-    //     };
-    //     chatService.pushMessage(message, new ChatComponent(chatService));
-    // });
+    
 
-    it("scrollToBottom should setTimeout", () => {
-        const chatElement = {
-            nativeElement: {
-                scrollTop: 0,
-                scrollHeight: 500,
-                clientHeight: 250
-            }
-        };
-        chatService.scrollToBottom(chatElement);
-        expect(chatElement).toBeDefined();
-    });
+    
+
+
 });
