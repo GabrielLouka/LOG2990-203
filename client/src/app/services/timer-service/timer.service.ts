de06a9fcb0950ec2b4cf762ee50fcb5000ed2748
MarieJadeMarcil <90798477+MarieJadeMarcil@users.noreply.github.com>
2023-04-16 14:32:48 -0400

    timer service coverage 100%

--- client/src/app/services/timer-service/timer.service.ts
+++ client/src/app/services/timer-service/timer.service.ts
@@ -1,58 +1,10 @@
-import { ElementRef, Injectable, OnDestroy } from '@angular/core';
-import { MILLISECOND_TO_SECONDS, MINUTE_LIMIT, MINUTE_TO_SECONDS } from '@common/utils/env';
+import { Injectable } from '@angular/core';
+import { MINUTE_TO_SECONDS } from '@common/utils/env';
 
 @Injectable({
     providedIn: 'root',
 })
-export class TimerService implements OnDestroy {
-    private timeInSeconds: number;
-    private shouldStop = false;
-    private interval: number;
-
-    get intervalTime() {
-        return this.interval;
-    }
-
-    get currentMinutes() {
-        return Math.floor(this.timeInSeconds / MINUTE_TO_SECONDS);
-    }
-
-    get currentSeconds() {
-        return Math.floor(this.timeInSeconds % MINUTE_TO_SECONDS);
-    }
-
-    get winningTimeInSeconds(): number {
-        return this.currentMinutes * MINUTE_TO_SECONDS + this.currentSeconds;
-    }
-
-    get winningTimeToString(): string {
-        return this.currentMinutes.toString() + ':' + this.currentSeconds.toString();
-    }
-
-    handleTickingTime(minute: ElementRef, second: ElementRef) {
-        this.interval = window.setInterval(() => {
-            if (this.timeInSeconds >= 0) {
-                this.ticToc(minute, second);
-            }
-        }, MILLISECOND_TO_SECONDS);
-    }
-
-    start() {
-        this.timeInSeconds = 0;
-    }
-
-    ngOnDestroy(): void {
-        window.clearInterval(this.interval);
-        this.shouldStop = false;
-        this.timeInSeconds = 0;
-    }
-
-    ticToc(minute: ElementRef, second: ElementRef) {
-        if (!this.shouldStop) this.timeInSeconds++;
-        minute.nativeElement.innerText = this.currentMinutes < MINUTE_LIMIT ? '0' + this.currentMinutes : this.currentMinutes;
-        second.nativeElement.innerText = this.currentSeconds < MINUTE_LIMIT ? '0' + this.currentSeconds : this.currentSeconds;
-    }
-
+export class TimerService {
     convertScoreToString(scoreTime: number): string {
         const formattedMinutes = Math.floor(scoreTime / MINUTE_TO_SECONDS).toLocaleString('en-US', {
             minimumIntegerDigits: 2,
@@ -63,9 +15,5 @@ export class TimerService implements OnDestroy {
             useGrouping: false,
         });
         return formattedMinutes + ':' + formattedSeconds;
-    }
-
-    stop() {
-        this.shouldStop = true;
     }
 }
