853460cc76b0c6995f32aa9412fd926f97209ab2
antiNT <32984684+antiNT2@users.noreply.github.com>
2023-04-16 00:25:55 -0400

    fixed cheat mode regression

--- client/src/app/classes/delayed-method/delayed-method.ts
+++ client/src/app/classes/delayed-method/delayed-method.ts
@@ -37,7 +37,6 @@ export class DelayedMethod {
     }
 
     pause() {
-        if (this.isExecuted) return; // we don't need to pause if it's already executed
         this.isPaused = true;
         if (this.timeoutId) {
             clearTimeout(this.timeoutId);
